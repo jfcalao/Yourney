82d763c86324b7323883026ee326f544a58fef65
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2021-06-06 15:01:16 -0500

    Where

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-import Home from '../pages/home';
-import Admin_Home from '../pages/Admin/Admin_Home';
+import Where from '../pages/Where';
 
 
-const routes = [
-    {
-        path: "/",
-        component: Home,
-        exact: true
-    },
-    {
-        path: "/admin",
-        component: Admin_Home,
-        exact: true
-    }
-=======
 //Layouts
 import Home from '../pages/home'
 import WhatToDo from '../pages/whatToDo'
@@ -30,7 +16,11 @@ const routes = [
     component: WhatToDo,
     exact: true,
   },
->>>>>>> ae54e3ca2bb3181d00ffb2da081b4252e7d5f53b
+  {
+    path: '/where',
+    component: Where,
+    exact: true,
+  }
 ]
 
 export default routes
