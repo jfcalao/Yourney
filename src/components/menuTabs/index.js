dd91ccb2de37121e25908fff51f1b3d48b5e6dd7
bjaider <bjaider@uninorte.edu.co>
2021-06-06 10:55:26 -0500

    Menu tabs active feature added

--- src/components/menuTabs/index.js
+++ src/components/menuTabs/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './style.css'
 
 import Food from '../../icons/food'
@@ -8,28 +8,38 @@ import City from '../../icons/city'
 import Party from '../../icons/party'
 
 const MenuTabs = () => {
+  /*   const [tab, setTab] = useState(initialState)
+  useEffect(() => {
+    document.getElementById(id);
+  }, []) */
+  const [tab, setTab] = useState('all')
+  function changeColor(id) {
+    document.getElementById(tab).classList.remove('active')
+    setTab(id)
+    document.getElementById(id).classList.add('active')
+  }
   return (
     <div className="menu-tabs-container">
-      <div>
+      <div className="active" onClick={() => changeColor('all')} id="all">
         <span>Todos</span>
       </div>
-      <div>
+      <div id="food" onClick={() => changeColor('food')}>
         <Food width="32px" height="32px" fill="white" />
         <span>Comida</span>
       </div>
-      <div>
+      <div id="drink" onClick={() => changeColor('drink')} >
         <Drink width="32px" height="32px" fill="white" />
         <span>Bebidas</span>
       </div>
-      <div>
+      <div id="culture" onClick={() => changeColor('culture')} >
         <Culture width="32px" height="32px" fill="white" />
         <span>Cultura</span>
       </div>
-      <div>
+      <div id="city" onClick={() => changeColor('city')} >
         <City width="32px" height="32px" fill="white" />
         <span>Sitios</span>
       </div>
-      <div>
+      <div id="party" onClick={() => changeColor('party')} >
         <Party width="32px" height="32px" fill="white" />
         <span>Fiesta</span>
       </div>
